4c9c388884e844fdf5ca1a31a99b781942c01f47
aevtushenko <aevtushenko@xiag.ch>
2016-07-22 13:04:56 +0600

    Add ability to set environment variables in JsonExecutorFactory.

--- src/Factory/JsonExecutorFactory.php
+++ src/Factory/JsonExecutorFactory.php
@@ -30,13 +30,22 @@ class JsonExecutorFactory
      * @param string $executablePath Executable path (eg "php", "node")
      * @param string $resultType Result type will be passed to "deserialize" as type (eg. "array<string>")
      * @param array $commandPrefixes Fixed command arguments (eg ["--verbose", "--no-interactive"])
+     * @param array $environmentVariables Environment variables (eg ["TZ" => "UTC"])
      * @return JsonCommandExecutor
      */
-    public function createJsonExecutor($executablePath, $resultType, array $commandPrefixes = [])
+    public function createJsonExecutor(
+        $executablePath,
+        $resultType,
+        array $commandPrefixes = [],
+        array $environmentVariables = []
+    )
     {
         $command = new JsonCommandExecutor($executablePath, $this->serializer, $resultType);
         foreach ($commandPrefixes as $prefix) {
             $command->addCommandPrefix($prefix);
+        }
+        foreach ($environmentVariables as $name => $value) {
+            $command->addEnvironmentVariable($name, $value);
         }
         return $command;
     }
